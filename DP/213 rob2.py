540c0acee4f47dfd1330e8dcc841fcfac4cbf90b
fly8764 <975533938@.qq.com>
2019-07-28 17:30:33 +0800

    7/28
    Signed-off-by: fly8764 <975533938@.qq.com>

--- DP/213 rob2.py
+++ DP/213 rob2.py
@@ -1,9 +1,4 @@
 class Solution:
-    #掐头去尾
-    #为了不受尾的影响，直接把尾 截掉，此时不用尾，头可用，可不用
-    #对等，为了不受头影响，直接把头去掉，此时不用考虑头，尾可用可不用，
-    #一共三种结果，取最大值，
-    #为了防止受影响，直接把头或者尾去掉一个
     def rob(self, nums):
         size = len(nums)
         if size == 0:
@@ -12,35 +7,28 @@ class Solution:
         if size < 4:
             return max(nums)
 
-        return max(self._rob(nums[0:-1]),self._rob(nums[1:]))
-
-    def _rob(self, nums):
-        size = len(nums)
-        if size == 0:
-            return 0
-
-        if size < 3:
-            return max(nums)
+        # if size == 3:
+        #     return max(nums)
 
         dp = [0] * size
         dp[0] = nums[0]
-        # dp[1]不是直接等于nums[1],而是max(nums[0],nums[1])
-        dp[1] = max(nums[0], nums[1])
+        dp[1] = nums[1]
+        dp[2] = nums[2] + nums[0]
 
-        for i in range(2, size):
-            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+        flag = [0] * size
+        flag[0] = 1
+        flag[1] = 0
+        flag[2] = 1
 
-        return dp[-1]
+        for i in range(3, size):
+            if dp[i - 2] > dp[i - 3]:
+                dp[i] = dp[i - 2] + nums[i]
+                flag[i] = flag[i - 2]
+            else:
+                dp[i] = dp[i - 3] + nums[i]
+                flag[i] = flag[i - 3]
 
-
-if __name__ == '__main__':
-    so = Solution()
-    res = so.rob([1,2,1,1])
-    print(res)
-
-    res = so.rob([1,1,1,2])
-    print(res)
-
-    res = so.rob([2,1,1,2])
-    print(res)
-
+        if dp[-1] > dp[-2] and flag[-1] != 0:
+            return dp[-1]
+        else:
+            return dp[-2]
