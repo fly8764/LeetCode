a406a8feaad6c9c9d9cb206cd2bb5acd09f56188
fly8764 <975533938@qq.com>
2019-08-13 23:49:42 +0800

    8/13.late

--- Tree/236 lowestCommonAncestor.py
+++ Tree/236 lowestCommonAncestor.py
@@ -5,6 +5,26 @@
 #         self.left = None
 #         self.right = None
 
+#这种方法 是 从底往上 递归
+#DFS(后序遍历) + 回溯法(现在认为回溯法就是 DFS在递归时 剪枝，返回)，本体是回溯 mid
+#当left,right,mid有两个为True时，结果就可以一定是 root.val
+#每次递归返回 当前有没有找到p or q，返回给上层 递归看
+#注意 题目中给的p，q是树节点，而不是值
+
+
 class Solution:
     def lowestCommonAncestor(self, root, p, q):
-        pass
+        self.res = None
+        self.dfs(root,p,q)
+        return self.res
+
+    def dfs(self,root,p,q):
+        if not root:
+            return 0
+        left = self.dfs(root.left,p,q)
+        right = self.dfs(root.right,p,q)
+        mid = root == p or root == q
+        if left + right + mid > 1:
+            self.res = root
+        return left or right or mid
+
