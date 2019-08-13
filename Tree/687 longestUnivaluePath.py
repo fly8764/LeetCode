23837e8002b0a18e38c40999580e56b4b6d87929
fly8764 <975533938@.qq.com>
2019-08-13 19:24:41 +0800

    8/13

--- Tree/687 longestUnivaluePath.py
+++ Tree/687 longestUnivaluePath.py
@@ -7,4 +7,26 @@
 
 class Solution:
     def longestUnivaluePath(self, root):
-        pass
+        self.res = 0
+        if not root:
+            return 0
+
+        def dfs(root):
+            if not root.left and not root.right:
+                return 0
+
+            temp_left,temp_right = 0,0
+            if root.left:
+                left = dfs(root.left)
+                if root.val == root.left.val:
+                    temp_left = left + 1
+
+            if root.right:
+                right =dfs(root.right)
+                if root.val == root.right.val:
+                    temp_right = right + 1
+            self.res = max(self.res,temp_left + temp_right)
+            return max(temp_left,temp_right)
+
+        dfs(root)
+        return self.res
