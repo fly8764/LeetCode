2c7871976cbadd4261663e36f6f3140289724287
fly8764 <975533938@qq.com>
2019-10-03 00:07:28 +0800

    add new solution

--- DP/120 minimumTotal.py
+++ DP/120 minimumTotal.py
@@ -1,5 +1,5 @@
 class Solution:
-    #自底向上 dp[i] = min(dp[i],dp[i+1]) + nums[i]
+    #自底向上 dp[i] = min(dp[i],dp[i+1]) + nums[i],使用o(n)的空间复杂度
     def minimumTotal(self, triangle):
         if not len(triangle) or not len(triangle[0]):
             return
@@ -32,9 +32,33 @@ class Solution:
     #         dp = cur
     #     return min(dp)
 
+class Solution1:
+    #从上到下，空间复杂度o(2n)
+    # def minimumTotal(self, triangle):
+    #     size = len(triangle)
+    #     if size < 1:return
+    #     dp = [triangle[0][0]]
+    #     for i in range(1,size):
+    #         cur = [dp[0]+triangle[i][0]]
+    #         for j in range(1,len(dp)):
+    #             cur.append(min(dp[j-1],dp[j]) + triangle[i][j])
+    #         cur.append(dp[-1]+ triangle[i][-1])
+    #         dp = cur[:]
+    #     return min(dp)
+
+    def minimumTotal(self,triangle):
+        size = len(triangle)
+        if size < 1:return
+        dp = triangle[-1]
+        for i in range(size-2,-1,-1):
+            for j in range(len(triangle[i])):
+                dp[j] = min(dp[j],dp[j+1]) + triangle[i][j]
+        return dp[0]
+
+
 
 if __name__ == '__main__':
-    so = Solution()
+    so = Solution1()
     triangle  = [
      [2],
     [3,4],
--- HashTable/523 checkSubarraySum.py
+++ HashTable/523 checkSubarraySum.py
@@ -1,22 +1,14 @@
-class Solution:
-    # 哈希表（取模）
-    # 使用字典，从左往右
-    # 求和，但是
-    # 字典的键不保存实际和，而是
-    # mod
-    # k，对应的值为
-    # 当前的下标i； 往右求和的过程中，当在此遇到与之前相同余数的值(下标j)
-    # 时，说明
-    # nums[i + 1:j + 1]
-    # 之间的数组串的实际和sum_
-    # 是
-    # k的整数倍
-    # k×n(n > 0);
-    # 所以，当j - i > 1
-    # 时，即可返回True。
-    #
-    # 因为，这里要求的是整除，所以只关心余数即可，取模(余)
-    # 满足结合律。
+'''
+哈希表（取模）
+使用字典，从左往右求和，但是字典的键不保存实际和，而是mod k对应的值为当前的下标i；
+往右求和的过程中，当在此遇到与之前相同余数的值(下标j)时，说明
+nums[i + 1:j + 1]之间的数组串的实际和sum_是k的整数倍k×n(n > 0);
+所以，当j - i > 1时，即可返回True。
+因为，这里要求的是整除，所以只关心余数即可，取模(余)满足结合律。
+
+这题主要使用的是数学方法，取余，而不是动态规划，动规咋做呢？
+'''
+class Solution1:
     def checkSubarraySum(self, nums, k):
         size = len(nums)
         if size < 2:
@@ -28,6 +20,7 @@ class Solution:
             temp += nums[i]
             if  k:
                 temp %= k
+            #这个用法很好
             pre = lookup.get(temp,None)
             if pre != None:
                 if i - pre > 1:
@@ -69,9 +62,33 @@ class Solution:
     #     return False
 
 
+class Solution:
+    def checkSubarraySum(self, nums, k):
+        size = len(nums)
+        if size < 2:return False
+        temp = 0
+        lookup = {}
+        #这是为了长度为2，且两个数均是k的倍数的情况考虑的；如果不这么做，两次取的下标分布为0，1，差值为1，无法得到结果
+        #所以要把起始下标设为-1，这样当总长度为2时，才能得出正确结果
+        lookup[0] = -1
+
+        for i in range(size):
+            temp += nums[i]
+            if k:
+                temp %= k
+            pre = lookup.get(temp,None)
+            if pre != None:
+                if i - pre > 1:
+                    return True
+            else:
+                lookup[temp] = i
+        return False
+
+
 if __name__ == '__main__':
     so = Solution()
-    nums = [23,2,4,6,7] #[23,2,6,4,7] [23,2,6,4,7]
+    nums = [1,24,6] #[23,2,6,4,7] [23,2,6,4,7]
+
     k =6
     res = so.checkSubarraySum(nums,k)
     print(res)
